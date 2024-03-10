3f4abe459453c604e445417d3869b050d66fbd33
Hsr19 <harshvardhanrathore3000@gmail.com>
2024-03-11 00:33:19 +0530

    added section 2

--- PAGE1/index.js
+++ PAGE1/index.js
@@ -1,5 +1,5 @@
 window.addEventListener('scroll', function () {
     const scrollValue = window.scrollY;
     const rocket = document.querySelector('.rocket');
-    rocket.style.transform = `translateX(${scrollValue * 1.5}px)`;
+    rocket.style.transform = `translate(${scrollValue * 1.5}px,${-scrollValue*0.5}px)`;
 });
